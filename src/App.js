3b8b3ce8e8de2dc4b9e49fcb208d491669dfdf1a
MarianoBF <mariano.b.fernandez@gmail.com>
2021-02-24 15:36:19 -0300

    insertar planilla, ajustes estilo

--- src/App.js
+++ src/App.js
@@ -5,69 +5,66 @@ import './App.css';
 
 function App() {
 
-const [nombre, setNombre] = useState("");
-const [apellido, setApellido] = useState("");
-const [edad, setEdad] = useState("");
-const [nacionalidad, setNacionalidad] = useState("");
-
+const [nombre, setNombre] = useState("Juan");
+const [apellido, setApellido] = useState("Pérez");
+const [edad, setEdad] = useState("60");
+const [nacionalidad, setNacionalidad] = useState("Argentina");
 
 const cambioNombre = (e) => {
- setNombre(e.target.value)
+  setNombre(e.target.value)
 }
 
 const cambioApellido = (e) => {
   setApellido(e.target.value)
- }
+}
 
  
- const cambioEdad = (e) => {
+const cambioEdad = (e) => {
   setEdad(e.target.value)
- }
+}
 
  
- const cambioNacionalidad = (e) => {
+const cambioNacionalidad = (e) => {
   setNacionalidad(e.target.value)
- }
+}
  
-
-
 const submiteador = (e) => {
-  e.preventDefault()
+  console.log(e)
+  //e.preventDefault()
   const datos = {
-    nombre: nombre,
-    apellido: apellido,
-    edad: edad,
-    nacionalidad: nacionalidad
+    Nombre: nombre,
+    Apellido: apellido,
+    Edad: edad,
+    Nacionalidad: nacionalidad
   }
   axios.post("https://sheet.best/api/sheets/001713ed-b1af-40b8-99a5-ceca0f58a844", datos)
     .then(response => {console.log(response)})
-  console.log(nombre, apellido, edad, nacionalidad)
 }
 
   return (
-<Container fluid>
-        <Header as='h1'>Completá la planilla para mandar los datos a Google Sheets</Header>
-        <Form onSubmit={submiteador}>
-          <Form.Field>
-            <label>Nombre</label>
-            <input placeholder='Ingresá nombre' name="nombre" onChange={cambioNombre}/>
-          </Form.Field>
-          <Form.Field>
-            <label>Apellido</label>
-            <input placeholder='Ingresá apellido' name="apellido" onChange={cambioEdad}/>
-          </Form.Field>
-          <Form.Field>
-            <label>Edad</label>
-            <input placeholder='Ingresá edad' name= "edad" onChange={cambioApellido}/>
-          </Form.Field>
-          <Form.Field>
-            <label>Nacionalidad</label>
-            <input placeholder='Ingresá Nacionalidad' name="nacionalidad" onChange={cambioNacionalidad} />
-          </Form.Field>
-          
-          <Button color="blue" type='submit'>Submit</Button>
-        </Form>
-      </Container>
+<Container className="contenedor">
+  <Header as='h2'>Completá la planilla para mandar los datos a Google Sheets</Header>
+  <Form onSubmit={submiteador}>
+    <Form.Field>
+      <label>Nombre</label>
+      <input placeholder='Juan' name="nombre" value={nombre} onChange={cambioNombre}/>
+    </Form.Field>
+    <Form.Field>
+      <label>Apellido</label>
+      <input placeholder='Perez' name="apellido" value={apellido} onChange={cambioApellido}/>
+    </Form.Field>
+    <Form.Field>
+      <label>Edad</label>
+      <input type="number" placeholder='60' name= "edad" max="120" min="1" onChange={cambioEdad}/>
+    </Form.Field>
+    <Form.Field>
+      <label>Nacionalidad</label>
+      <input placeholder='Argentina' name="nacionalidad" onChange={cambioNacionalidad} />
+    </Form.Field>
+    
+    <Button color="blue" type='submit'>Submit</Button>
+  </Form>
+</Container>
   );
 }
 
