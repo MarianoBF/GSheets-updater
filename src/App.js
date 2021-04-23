636e4bb18b8a7b6d007f4ae7b9cb00d1e9d9aeb7
Mariano Fernandez <mariano.b.fernandez@gmail.com>
2021-04-23 13:03:05 -0300

    rearrange format & reload to show updates

--- src/App.js
+++ src/App.js
@@ -1,68 +1,102 @@
-import { Button, Form, Container, Header } from 'semantic-ui-react'
-import {useState} from "react"
-import axios from "axios"
-import './App.css';
-import IframeGS from "./components/IframeGS"
+import {Button, Form, Container, Header} from "semantic-ui-react";
+import {useState} from "react";
+import axios from "axios";
+import "./App.css";
+import IframeGS from "./components/IframeGS";
 
 function App() {
+  const sampleValues = {
+    nombre: "Juan",
+    apellido: "Pérez",
+    edad: 50,
+    nacionalidad: "keniata",
+  };
 
-const sampleValues = {
-  nombre: "Juan",
-  apellido: "Pérez",
-  edad: 50,
-  nacionalidad: "keniata"
-}
+  const [renglon, setRenglon] = useState(sampleValues);
 
-const [renglon, setRenglon] = useState(sampleValues);
+  const handleInput = e => {
+    const {name, value} = e.target;
+    setRenglon({...renglon, [name]: value});
+  };
 
-const handleInput = (e) => {
-  const {name, value} = e.target;
-  setRenglon({...renglon, [name]: value})
-}
-
-const submiteador = (e) => {
-  console.log(e)
-  //e.preventDefault()
-  const datos = {
-    Nombre: renglon.nombre,
-    Apellido: renglon.apellido,
-    Edad: renglon.edad,
-    Nacionalidad: renglon.nacionalidad
-  }
-  axios.post("https://sheet.best/api/sheets/001713ed-b1af-40b8-99a5-ceca0f58a844", datos)
-    .then(response => {
-      console.log(response)
+  const submiteador = e => {
+    console.log(e);
+    const datos = {
+      Nombre: renglon.nombre,
+      Apellido: renglon.apellido,
+      Edad: renglon.edad,
+      Nacionalidad: renglon.nacionalidad,
+    };
+    axios
+      .post(
+        "https://sheet.best/api/sheets/001713ed-b1af-40b8-99a5-ceca0f58a844",
+        datos
+      )
+      .then(response => {
+        console.log(response)
+        window.location.reload();
       })
-}
+      .catch(error => {
+        console.log(error)
+      })
+  };
 
-  return (<main>
-<Container className="contenedor">
-  <Header as='h2'>Completá la planilla para mandar los datos a Google Sheets</Header>
-  <Form onSubmit={submiteador}>
-    <Form.Field>
-      <label>Nombre</label>
-      <input placeholder='Juan' name="nombre" value={renglon.nombre} onChange={handleInput}/>
-    </Form.Field>
-    <Form.Field>
-      <label>Apellido</label>
-      <input placeholder='Perez' name="apellido" value={renglon.apellido} onChange={handleInput}/>
-    </Form.Field>
-    <Form.Field>
-      <label>Edad</label>
-      <input type="number" placeholder='60' name= "edad" max="120" min="1" value={renglon.edad} onChange={handleInput}/>
-    </Form.Field>
-    <Form.Field>
-      <label>Nacionalidad</label>
-      <input placeholder='Argentina' name="nacionalidad" value={renglon.nacionalidad} onChange={handleInput} />
-    </Form.Field>
-    
-    <Button color="blue" type='submit'>Submit</Button>
-  </Form>
-</Container>
+  return (
+    <main>
+      <Container className="contenedor">
+        <Header as="h2">
+          Completá la planilla para mandar los datos a Google Sheets
+        </Header>
+        <Form onSubmit={submiteador}>
+          <Form.Field>
+            <label>Nombre</label>
+            <input
+              placeholder="Juan"
+              name="nombre"
+              value={renglon.nombre}
+              onChange={handleInput}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Apellido</label>
+            <input
+              placeholder="Perez"
+              name="apellido"
+              value={renglon.apellido}
+              onChange={handleInput}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Edad</label>
+            <input
+              type="number"
+              placeholder="60"
+              name="edad"
+              max="120"
+              min="1"
+              value={renglon.edad}
+              onChange={handleInput}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Nacionalidad</label>
+            <input
+              placeholder="Argentina"
+              name="nacionalidad"
+              value={renglon.nacionalidad}
+              onChange={handleInput}
+            />
+          </Form.Field>
 
-<IframeGS />
+          <Button color="blue" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
 
-</main>
+      <IframeGS/>
+
+   </main>
   );
 }
 
--- src/components/IframeGS.js
+++ src/components/IframeGS.js
@@ -1,7 +1,18 @@
-function IframeGS() {
-    return(
-        <iframe title="Spreadsheet" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vR1BL_7ww4-b-r1vm1Qa-b1kPY4Mc1Vy0ZUFC4u06uJ3jebw89ZlXvmWpy41iezHrFNnqAMegemrgkG/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
-    ) 
+function IframeGS(props) {
+  return (
+    <div>
+      {/* <iframe
+      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vR1BL_7ww4-b-r1vm1Qa-b1kPY4Mc1Vy0ZUFC4u06uJ3jebw89ZlXvmWpy41iezHrFNnqAMegemrgkG/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"></iframe> */}
+      <iframe
+        title="Spreadsheet"
+        src="https://docs.google.com/spreadsheets/d/1xOJx9ZrYKz2KWw5l40cZ8xJueZGm9XQzHXOdv0dLzFM/htmlembed/sheet?gid=0&range=A1:G35"
+        frameborder="1"
+        marginheight="0"
+        marginwidth="0">
+        {" "}
+      </iframe>
+    </div>
+  );
 }
 
 export default IframeGS;
